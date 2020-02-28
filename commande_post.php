70d06699ef7ae1c2eca15c3391ac8efd2c187957
meagane-takacs <meagane.takacs@le-campus-numerique.fr>
2020-02-28 14:02:30 +0100

    Maj panier 3

--- commande_post.php
+++ commande_post.php
@@ -3,14 +3,13 @@
 include('functions_BDD.php');
 include('functions.php');
 session_start();
-var_dump($_SESSION);
+//var_dump($_SESSION);
 
 $bdd = connection();
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Catalogue</title>
     <meta charset="utf-8"/>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
           integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
@@ -18,11 +17,18 @@ $bdd = connection();
 </head>
 <body>
 
-<div class="cmdOk"><?php
-  echo "Votre commande a bien été prise en compte";
-  ?>
+
+<div class="cmdOk">
+<p>
+    Merci, votre commande à bien été prise en compte
+</p>
 </div>
+
+<form method="POST" action="catalogue.php">
+    <input class="submit" type="submit" value="Retour au catalogue">
+</form>
 
 
 </body>
+</html>
 
